45b5d82f5a2d67f605be5c4e77dc3f79b3c19a43
Ingrid Cardoso <franata.0715@gmail.com>
2026-06-04 21:16:59 -0500

    informacion main.js

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,24 @@
-document.addEventListener("DOMContentLoaded" (Event) => {
+document.addEventListener("DOMContentLoaded", (Event) => {
 // alert("Bienvenido a Claramente")
 });
-document.addEventListener("submit" (Event) => {
-alert(Formulario enviado);
+document.addEventListener("submit", (event) => {
+ if (event.target && event.target.id === "formlogin") {
+event.preventDefault();
+login();
+ }
+    
+});
 
-})
+function login() {
+    console.log("estamos en la funcion login");
+    const email = document.getElementById("correo").value;
+    const password = document.getElementById("password").value;
+
+    console.log("Correo:", correo);
+    console.log("Contraseña:", password);
+}
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    login();
+});
