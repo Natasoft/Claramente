139a6c9e5197c1044fe256d0466bbb08cb068040
Ingrid Cardoso <franata.0715@gmail.com>
2026-06-17 16:20:43 -0500

    Validacion de inicio con expresiones regulares

--- JS/inicio.js
+++ JS/inicio.js
@@ -12,5 +12,9 @@ const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     }
     if (password.length < 6) {
         alert("La contraseña debe tener al menos 6 caracteres.");
+        const datos = {correo: email, password: password};
+        console.log(datos);
+        return;
+    }
     alert(`email: ${email}\npassword: ${password}`);
 }
