00adad41f541f1cbe79e197667afd7416a0335e2
Ingrid Cardoso <franata.0715@gmail.com>
2026-06-16 21:49:00 -0500

    regex validar email

--- JS/inicio.js
+++ JS/inicio.js
@@ -1,9 +1,16 @@
 export function login() {
 console.log("estamos en la funcion login");
+const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     const email = document.getElementById("correo").value;
     const password = document.getElementById("password").value;
 
     console.log("correo:", email);
     console.log("Contraseña:", password);
+    if (!regex.test(email)) {
+        alert("Por favor, ingresa un correo electrónico válido.");
+        return;
+    }
+    if (password.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres.");
     alert(`email: ${email}\npassword: ${password}`);
 }
