0614ea5bf9f1a2d51688ba00a795227a9f3ba48d
Ingrid Cardoso <franata.0715@gmail.com>
2026-06-17 21:03:25 -0500

    Control codigo fetch

--- JS/inicio.js
+++ JS/inicio.js
@@ -10,11 +10,35 @@ const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
         alert("Por favor, ingresa un correo electrónico válido.");
         return;
     }
-    if (password.length < 6) {
-        alert("La contraseña debe tener al menos 6 caracteres.");
-        const datos = {correo: email, password: password};
-        console.log(datos);
-        return;
+    if (password.length < 4) {
+        alert("La contraseña debe tener al menos 4 caracteres.");
+        const datos = {params: {correo: email, password: password}, "url": "http://localhost:8080/login", "method": "POST"};
+        console.log("datos a enviar", datos);
+        enviarpeticion(datos);
+    
     }
-    alert(`email: ${email}\npassword: ${password}`);
-}
+    function enviarpeticion(datos) {
+       let {params, url, method} = datos;
+        console.log("params:", params);
+        console.log("url:", url);
+        console.log("method:", method);
+        try {
+        fetch(url, {
+            method: method,
+            headers: {  
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Respuesta del servidor:", data);   
+        })
+        .catch(error => {
+        console.error("Error al enviar la peticion:", error);
+        });
+    } catch(error){
+        console.error("Error en la funcion enviarpeticion:", error);
+         }
+    }
+ }
