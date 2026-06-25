c85f6f885ce32baf46b9e2e02a4a64a762efbed2
Ingrid Cardoso <franata.0715@gmail.com>
2026-06-24 21:12:55 -0500

    encriptando contraseñas

--- JS/inicio.js
+++ JS/inicio.js
@@ -3,43 +3,42 @@ console.log("estamos en la funcion login");
 const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     const email = document.getElementById("correo").value;
     const password = document.getElementById("password").value;
+    const hashedPassword = md5(password);
 
     console.log("correo:", email);
-    console.log("Contraseña:", password);
+    console.log("Contraseña:", hashedPassword);
     if (!regex.test(email)) {
         alert("Por favor, ingresa un correo electrónico válido.");
         return;
     }
     if (password.length < 4) {
         alert("La contraseña debe tener al menos 4 caracteres.");
-        const datos = {params: {correo: email, password: password}, "url": "http://localhost:8080/login", "method": "POST"};
+        return;
+    }
+        const datos = {params: {correo: email, password: hashedPassword}, "url": "http://localhost:8080/login", "method": "POST"};
         console.log("datos a enviar", datos);
+        console.log("Voy a ejecutar fetch");
         enviarPeticion(datos);
-    
+        //enviarPeticionGET({params: {correo: email, password: hashedPassword}, "url": "http://localhost:8080/login", "method": "GET"});
+        console.log("md5 existe?", typeof md5);
     }
     function enviarPeticion(datos) {
-         console.log("Entre a enviarPeticion");
-       let {params, url, method} = datos;
-        console.log("params:", params);
-        console.log("url:", url);
-        console.log("method:", method);
-        try {
-        fetch(url, {
-            method: method,
-            headers: {  
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(params)
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log("Respuesta del servidor:", data);   
-        })
-        .catch(error => {
-        console.error("Error al enviar la peticion:", error);
-        });
-    } catch(error){
-        console.error("Error en la funcion enviarPeticion:", error);
-         }
-    }
- }
+
+    const { params, url, method } = datos;
+
+    fetch(url, {
+        method,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(params)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Respuesta:", data);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+
+}
