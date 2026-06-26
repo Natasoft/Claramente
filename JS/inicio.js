853166e6032626665dd9b1d665b91e1764fa562e
Ingrid Cardoso <franata.0715@gmail.com>
2026-06-25 21:24:00 -0500

    Organizando el MD5 en la aplicacion

--- JS/inicio.js
+++ JS/inicio.js
@@ -17,28 +17,39 @@ const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     }
         const datos = {params: {correo: email, password: hashedPassword}, "url": "http://localhost:8080/login", "method": "POST"};
         console.log("datos a enviar", datos);
-        console.log("Voy a ejecutar fetch");
-        enviarPeticion(datos);
-        //enviarPeticionGET({params: {correo: email, password: hashedPassword}, "url": "http://localhost:8080/login", "method": "GET"});
-        console.log("md5 existe?", typeof md5);
+        //console.log("Voy a ejecutar fetch");
+        await enviarPeticion({
+            url: "http://localhost:8080/login",
+            method: "POST",
+            params: {correo: email, password: hashedPassword},
+            Fsuccess: function (response) => {
+                if (response.code === 200) {
+                    alert("Inicio de sesión exitoso");
+                    ir (index.html);
+                }
+                else {alert(response.message);}
+        }
+
+        );
+        await enviarPeticionGET({params: {correo: email, password: hashedPassword}, "url": "http://localhost:8080/login", "method": "GET"});
+        //console.log("md5 existe?", typeof md5);
     }
-    function enviarPeticion(datos) {
 
-    const { params, url, method } = datos;
-
-    fetch(url, {
-        method,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(params)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Respuesta:", data);
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
+    async function enviarPeticion(info) {
+        let {url, method, params, fSucces } = info, headers = {contentType: "application/json"};
+        if (method === "PATCH") headers = {contentType: "multipart/form-data"};
+        if (params !== undefined && method === "GET") url += "?" + new URLSearchParams(params);
+        if (method === "GET") method ={ method, headers, params};
+        if (method === "POST" || method === "PUT" || method === "DELETE" || method === "PATCH" ) method = { method, headers, body: JSON.stringify(params)};
+        
+        TRY {
+            console.log(url, method);
+            let resp = await fetch(url, method);
+            if (resp.ok) throw { status: resp.status, statusText: resp.statusText };
+            let respJson = await resp.json();
+            Fsuccess(respJson);
+        } catch (err) {
+                Fsuccess({code: err.status, message: err.message});
+        }
 
 }
