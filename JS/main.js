ae4975a80689243d8ddbe8155e993b36ee888d0f
Ingrid Cardoso <franata.0715@gmail.com>
2026-07-22 21:56:51 -0500

    Organizando el menu

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,27 @@
 import { login } from "./inicio.js";
 document.addEventListener("DOMContentLoaded", (Event) => {
-// alert("Bienvenido a Claramente")
+    // alert("Bienvenido a Claramente")
 });
 document.addEventListener("submit", (event) => {
- if (event.target && event.target.id === "formlogin") {
-event.preventDefault();
-login();
- }
-    
-});    
+    if (event.target && event.target.id === "formlogin") {
+        event.preventDefault();
+        login();
+    }
+
+});
 
 document.addEventListener("click", (e) => {
     console.log("e.target");
+});
+
+const submenu = document.querySelectorAll(".submenu");
+
+submenu.forEach(item => {
+
+    item.querySelector(".submenu-btn").addEventListener("click", () => {
+
+        item.classList.toggle("abierto");
+
+    });
+
 });
